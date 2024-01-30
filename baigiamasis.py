5f68f4241d2a6a16a91ba24be39c664be8832de3
Inga2024 <157992890+Inga2024@users.noreply.github.com>
2024-01-30 15:16:16 +0200

    savo faile sujungiau viska i viena

--- baigiamasis.py
+++ baigiamasis.py
@@ -244,6 +244,16 @@ dfbendrasbaltic.to_csv('Bendrasbaltic.csv', index=False)
 
 dfbendras = pd.concat([dfbendrasbaltic, dfbendraspanorama], axis=0)
 dfbendras.to_csv('Bendraskeloniu.csv', index=False)
-print(dfbendras)
+#print(dfbendras)
 
 
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+Vid.kaina pagal salis
+plt.figure(figsize=(12, 6))
+sns.barplot(x='Salis', y='Kaina', data=baltic_df_pasirinktos_salys)
+plt.title('Vidutine kaina pagal salis (Bendraskelioniu)')
+plt.show()
